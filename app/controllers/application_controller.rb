4cc1d4ef9bb4b097000c5aae74ba6e5b10e4514f
vagrant <vagrant@localhost.localdomain>
2020-07-09 10:45:45 +0000

    [Add]RegistrationsNewAction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      added_attrs = [:id, :name, :email, :password, :password_confirmation]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :log_in, keys: added_attrs
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,22 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all
   devise_scope :user do
     # 新規登録画面表示
-    get  'users/sign_up',       to: 'devise/registrations#new',     as: :new_user_registration
+    get  'users/sign_up',       to: 'users/registrations#new',     as: :new_user_registration
     # 新規登録
-    post 'users',               to: 'devise/registrations#create',  as: :user_registration
+    post 'users',               to: 'users/registrations#create',  as: :user_registration
     # ユーザ削除
-    delete 'users',             to: 'devise/registrations#destroy', as: :destroy_user_registration
+    delete 'users',             to: 'users/registrations#destroy', as: :destroy_user_registration
 
     # ログイン画面表示
-    get 'users/log_in',         to: 'devise/sessions#new',          as: :new_user_session
+    get 'users/log_in',         to: 'users/sessions#new',          as: :new_user_session
     # ログイン
-    post 'users/log_in',        to: 'devise/sessions#create',       as: :user_session
+    post 'users/log_in',        to: 'users/sessions#create',       as: :user_session
     # ログアウト
-    delete 'users/log_out',     to: 'devise/sessions#destroy',      as: :destroy_user_session
+    delete 'users/log_out',     to: 'users/sessions#destroy',      as: :destroy_user_session
 
     # パスワード変更画面を表示
-    get '/users/password/edit', to: 'devise/passwords#edit',        as: :edit_user_password
+    get '/users/password/edit', to: 'users/passwords#edit',        as: :edit_user_password
     # パスワードを変更
-    patch '/users/password',    to: 'devise/passwords#update',      as: :user_password
+    patch '/users/password',    to: 'users/passwords#update',      as: :user_password
   end
 end
