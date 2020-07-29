4cbe30f46e8106f66b91e224a95eb3262ec80db2
vagrant <vagrant@localhost.localdomain>
2020-07-29 00:36:53 +0000

    [Update]config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Katakanaeigo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = true
 
     config.time_zone = 'Tokyo'
 
